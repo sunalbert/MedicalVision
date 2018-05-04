e545123e6dc033855deebeebb11520a7522412a7
Albert <jasonjinquan@gmail.com>
2018-05-04 13:56:31 +0800

    add dice metric

---
+++ app/metrics/dice.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import numpy as np
+
+
+def dice_score(prediction: np.ndarray, target: np.ndarray) -> float:
+    """
+
+    :param prediction:
+    :param target:
+    :return:
+    """
+    pass
+
+
+def masked_dice_score(prediction: np.ndarray, target: np.ndarray, mask: np.ndarray) -> float:
+    """
+
+    :param prediction:
+    :param target:
+    :param mask:
+    :return:
+    """
+    pass
