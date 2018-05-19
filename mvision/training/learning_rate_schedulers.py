21913e8d45ef3815be1f1ef16120aead49d2fe1c
sunalbert <malcolm_sun@163.com>
2018-05-19 21:29:38 +0800

    update training package

---
+++ mvision/training/learning_rate_schedulers.py
@@ -0,0 +1,89 @@
+"""
+This file is borrowed from Allennlp
+
+`PyTorch learning rate schedulers <http://pytorch.org/docs/master/optim.html#how-to-adjust-learning-rate>`_,
+with a thin wrapper to allow registering them and instantiating them ``from_params``.
+
+The available learning rate schedulers are
+
+* `"step" <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.StepLR>`_
+* `"multi_step" <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.MultiStepLR>`_
+* `"exponential" <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.ExponentialLR>`_
+* `"reduce_on_plateau" <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.ReduceLROnPlateau>`_
+* `"cosine" <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.CosineAnnealingLR>`_
+"""
+
+import torch.optim.lr_scheduler
+from overrides import overrides
+from mvision.common.checks import ConfigurationError
+from mvision.common.params import Params
+from mvision.common.registrable import Registrable
+
+
+class LearningRateScheduler(Registrable):
+    """
+    This class just allows us to implement ``Registrable`` for Pytorch :class:`LRSchedulers`.
+    """
+    def __init__(self, lr_scheduler) -> None:
+        self.lr_scheduler = lr_scheduler
+
+    @classmethod
+    def from_params(cls, optimizer: torch.optim.Optimizer, params: Params):
+        scheduler = params.pop_choice("type", LearningRateScheduler.list_available())
+
+        schedulers = LearningRateScheduler.by_name(scheduler)(optimizer, **params.as_dict())  # type: ignore
+        if isinstance(schedulers, torch.optim.lr_scheduler.ReduceLROnPlateau):
+            return LearningRateWithMetricsWrapper(schedulers)
+        else:
+            return LearningRateWithoutMetricsWrapper(schedulers)
+
+    def step(self, metrics, epoch=None):
+        raise NotImplementedError
+
+    def step_batch(self, batch_num_total: int):
+        if batch_num_total is not None:
+            if hasattr(self.lr_scheduler, 'step_batch'):
+                self.lr_scheduler.step_batch(batch_num_total)
+            return
+
+
+class LearningRateWithoutMetricsWrapper(LearningRateScheduler):
+    """
+    A wrapper around learning rate schedulers that do not require metrics
+    """
+    def __init__(self, lr_scheduler: torch.optim.lr_scheduler._LRScheduler) -> None:  # pylint: disable=protected-access
+        super().__init__(lr_scheduler)
+        self.lr_scheduler = lr_scheduler
+
+    @overrides
+    def step(self, metrics, epoch=None):
+        self.lr_scheduler.step(epoch)
+
+
+class LearningRateWithMetricsWrapper(LearningRateScheduler):
+    """
+    A wrapper around learning rate schedulers that require metrics,
+    At the moment there is only a single instance of this lrs. It is the ReduceLROnPlateau
+    """
+    def __init__(self, lr_scheduler: torch.optim.lr_scheduler.ReduceLROnPlateau) -> None:
+        super().__init__(lr_scheduler)
+        self.lr_scheduler = lr_scheduler
+
+    @overrides
+    def step(self, metrics, epoch=None):
+        if not metrics:
+            raise ConfigurationError("The reduce_on_plateau learning rate scheduler requires "
+                                     "a validation metric to compute the schedule and therefore "
+                                     "must be used with a validation dataset.")
+        self.lr_scheduler.step(metrics, epoch)
+
+
+# We just use the Pytorch LRSchedulers, so here we force them into
+# Registry._registry so we can build them from params.
+Registrable._registry[LearningRateScheduler] = {   # pylint: disable=protected-access
+        "step": torch.optim.lr_scheduler.StepLR,
+        "multi_step": torch.optim.lr_scheduler.MultiStepLR,
+        "exponential": torch.optim.lr_scheduler.ExponentialLR,
+        "reduce_on_plateau": torch.optim.lr_scheduler.ReduceLROnPlateau,
+        "cosine": torch.optim.lr_scheduler.CosineAnnealingLR,
+}
